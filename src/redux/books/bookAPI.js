a6707b4a2e9d7239454197fa373b2b8835c6e8fe
Dennis <vigehi2017@gmail.com>
2022-09-30 21:50:04 +0300

    Added styling to my book store

--- src/redux/books/bookAPI.js
+++ src/redux/books/bookAPI.js
@@ -1,5 +1,6 @@
 export const getBook = async () => {
   const res = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0RZZEomj84t07BwtOyZA/books');
+  console.log(res);
   const data = await res.json();
   return data;
 };
@@ -15,7 +16,7 @@ export const postBook = async (books) => {
 };
 
 export const deleteBook = async (id) => {
-  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0RZZEomj84t07BwtOyZA/books${id}`,
+  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/0RZZEomj84t07BwtOyZA/books/${id}`,
     {
       method: 'DELETE',
     });
